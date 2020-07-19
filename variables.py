0b348beed34485f87406690398e49e662042b7cc
Vikas Singh Rathore <vikas.singh5988@gmail.com>
2020-07-19 16:52:48 +0530

    tuts7

---
+++ variables.py
@@ -0,0 +1,16 @@
+var1=12
+var2=22.33
+var3=" Hello "
+var4="101"
+var5="World "
+print("data type of  variable var3 is ", type(var3))
+print(var1+var2)
+print(str(var1)+var3)
+print(var3 + var5)
+print("enter first number")
+num1=input()
+print("enter second number")
+num2=input()
+sum=int(num1)+int(num2)
+print("sum  of the given number is",sum)
+print(5 * "Repeat\n")
