45bdad7d0111f4f90254f01d95f284e9bb6b7e34
Vikas Singh Rathore <vikas.singh5988@gmail.com>
2020-07-19 15:25:09 +0530

    tuts6

---
+++ tuts6.py
@@ -0,0 +1,13 @@
+#This is a single line comment
+print("Learning comment and escape sequence.....")
+
+"""
+This is 
+multi line
+comment
+"""
+print("I want to become a python developer","and expert in Web development as well", end=" ")
+print("and  Devops enggineer also")
+
+#Escape sequence
+print("I have my data stored on E:\\newfolder")
